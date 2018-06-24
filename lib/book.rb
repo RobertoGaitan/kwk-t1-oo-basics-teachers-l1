12ccf9214720114e545933aa17b4f809eebb3cdc
Roberto Gaitan <rgaitan.it@gmail.com>
2018-06-24 17:29:42 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -20,5 +20,21 @@ class Book
     @author
   end
   
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
 end
 
